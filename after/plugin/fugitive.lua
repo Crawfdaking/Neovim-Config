1f279e81422794cbfcc2461b13a41adb14f6e47f
Crawford <crawfordlee03@gmail.com>
2023-02-17 21:45:18 -0800

    Updated keybindings

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -2,10 +2,10 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
 local bufnr = vim.api.nvim_get_current_buf()
 local opts = {buffer = bufnr, remap = false}
-vim.keymap.set("n", "<leader>p", function()
+vim.keymap.set("n", "<leader>gp", function()
     vim.cmd.Git('push')
 end, opts)
 
 -- NOTE: It allows me to easily set the branch i am pushing and any tracking
 -- needed if i did not set the branch up correctly
-vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
+vim.keymap.set("n", "<leader>gt", ":Git push -u origin ", opts);
